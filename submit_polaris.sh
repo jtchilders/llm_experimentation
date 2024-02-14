2005278b07ff0b815d4ef61f4ce9ab435b7e322e
Taylor Childers <taylorchilders@gmail.com>
2024-02-14 08:08:07 -0600

    update submit script

--- submit_polaris.sh
+++ submit_polaris.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #PBS -l walltime=01:00:00
-#PBS -l select=2
+#PBS -l select=8
 #PBS -A datascience
 #PBS -l filesystems=home:eagle
-#PBS -q debug
+#PBS -q debug-scaling
 #PBS -o results/submit_polaris.stdout
 #PBS -e results/submit_polaris.stderr
 
@@ -26,7 +26,7 @@ echo starting job: $FILENAME_DATE
    echo NUM_GPUS_PER_NODE: $NUM_GPUS_PER_NODE
    echo NUM_NODES: $NUM_NODES
    export HDF5_DISABLE_VERSION_CHECK=1
-   mpiexec -n $TOTAL_MPI_RANKS -ppn $NUM_GPUS_PER_NODE --hostfile $PBS_NODEFILE python main.py -o results/$FILENAME_DATE -b 4096 -c 4 -e 5000
+   mpiexec -n $TOTAL_MPI_RANKS -ppn $NUM_GPUS_PER_NODE --hostfile $PBS_NODEFILE python main.py -o results/$FILENAME_DATE -b 1024 -c 6 -e 1000 -i 50 -l 0.05 --warmup-steps 5000
 
 } 1> results/$FILENAME_DATE.out 2> results/$FILENAME_DATE.err
 echo finished job: $FILENAME_DATE  time: $SECONDS
